e7ea9615e0b94a6135e691d5266a7d908932a5c2
gelwg1 <gelwg1@gmail.com>
2022-03-07 14:33:51 +0700

    add Practice function, landing page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from "react-router-dom";
 
 
 export default function Dashboard() {
@@ -15,7 +16,8 @@ export default function Dashboard() {
                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, nesciunt molestiae ex inventore quibusdam id architecto quos cupiditate nobis magnam eum voluptatem quas quis obcaecati dolor vero veritatis similique alias.
                   </h6>
                       <br />
-                      <h6><a className="btn btn-dark start start-two" href="#">Get Started</a></h6>
+                      
+                      <h6><Link className="btn btn-dark start start-two" to="/New_Flash/createcard">Get Started</Link></h6>
                   </div>
                   <div className="col-sm-9 showcase-img">
                       {/* <div className="circle"></div> */}
